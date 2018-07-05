fc6f7e33bf6abc0d3d4dd0cb1dd321dcced24b39
ferge22 <rytisspitrys@gmail.com>
2018-07-05 10:06:51 +0300

    clicking/unclicking like works add/deletes from obj

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView' //searchView is an object witch has all exported variables
 import * as recipeView from './views/recipeView'
 import * as listView from './views/listView'
@@ -144,8 +145,29 @@ elements.shopping.addEventListener('click', e => {
 });
 
 /*----Like CONTROLLER----*/
-const likesControler = () => {
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
 
+    if(!state.likes.isLiked(currentID)){
+        //Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        )
+        //Toggle the like button
+
+        //Add like to Ui list
+        console.log(state.likes);
+    }else{
+        //Remove like from the state
+        state.likes.deleteLike(currentID);
+        //Toggle the like button
+
+        //Remove like from Ui list
+    }
 
 };
 
@@ -164,7 +186,11 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
+        //Add ingredients to shopping list
         controlList();
+    }else if(e.target.matches('.recipe__love, .recipe__love *')){
+        //Like Controller
+        controlLike();
     }
 
     // console.log(state.recipe);
